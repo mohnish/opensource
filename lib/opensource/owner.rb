ec52a660a6c8bd16dc14e2c01e65970feb564fd4
Mohnish Thallavajhula <i@mohni.sh>
2014-08-10 18:36:32 -0700

    add owner to set and get user credentials

---
+++ lib/opensource/owner.rb
@@ -0,0 +1,33 @@
+module Opensource
+  module Owner
+    extend self
+
+    def set_credentials
+      puts "Enter full name: "
+      name = gets.chomp
+      puts "Enter email address: "
+      email = gets.chomp
+
+      f = File.new(File.expand_path("~/.osrc"), "w")
+      f.write <<-CREDENTIALS
+        name: #{name}
+        email: #{email}
+      CREDENTIALS
+      f.close
+    end
+
+    def get_credentials
+      user = {}
+
+      IO.foreach(File.expand_path("~/.osrc")) do |line|
+        current_line = line.strip
+        if !current_line.empty?
+          key, value = current_line.split(':')
+          user[key.strip] = value.strip
+        end
+      end
+
+      user
+    end
+  end
+end
