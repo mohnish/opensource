6d7350ba6bf1721925b8935ef3044ebcd0fd7c61
Mohnish Thallavajhula <i@mohni.sh>
2015-06-02 12:10:57 -0700

    refactor lib to separate out requesting from owner

--- lib/open_source.rb
+++ lib/open_source.rb
@@ -7,4 +7,18 @@ module OpenSource
   LICENSES = Dir.entries(File.expand_path("../../templates", __FILE__)).map do |filename|
     File.basename(filename, '.erb') if !['.', '..'].include?(filename)
   end.compact
+
+  def self.request_owner_credentials
+    puts "Enter full name: "
+    name = gets.chomp
+    puts "Enter email address: "
+    email = gets.chomp
+
+    { name: name, email: email }
+  end
+
+  def self.setup_owner_credentails
+    owner_credentials = request_owner_credentials
+    Owner.set_credentials(owner_credentials)
+  end
 end
